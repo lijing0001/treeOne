c3132befaf0482e6f4ee12c61213f877350235f4
lijing01 <lijing@raymingtech.com>
2018-03-09 16:28:12 +0800

    tabclick

--- src/app/tab/tab.component.ts
+++ src/app/tab/tab.component.ts
@@ -11,15 +11,15 @@ export class TabComponent {
   }
   public list = [
     {
-      id: 1,
+      id: '1',
       name: 'tab1',
       text: 'tab1内容'
     }, {
-      id: 2,
+      id: '2',
       name: 'tab2',
       text: 'tab2内容'
     }, {
-      id: 3,
+      id: '3',
       name: 'tab3',
       text: 'tab3内容'
     },
@@ -41,29 +41,47 @@ export class TabComponent {
   public navTop: string = '';
   public navLeft: string = '';
   public rowData: Object = {};
+  public selectTabIndex: number = 0;
   @Input() modal: any;
   public checkTab(index) {
     this.active = index;
   }
 
-  public onRightMenus(e, tab) {
+  public onRightMenus(e, i) {
     event.preventDefault();
-    console.log(e, tab)
+    this.selectTabIndex = i;
     if (e.which === 3) {
       this.navTop = e.clientY + 'px';
       this.navLeft = e.clientX + 'px';
-      this.rowData = tab;
       //控制右键动态变化
       jQuery('#rightMenusNav').css('display', 'block');
     }
   }
 
-  public btnRightClick(btnKey: string) {
+  public btnRightClick(btnKey: string,index) {
     let menuObj = {};
     menuObj['btnKey'] = btnKey;
     menuObj['rowData'] = this.rowData;
     jQuery('#rightMenusNav').css('display', 'none');
-    console.log(menuObj);
+    if(btnKey == 'add'){
+      let name = window.prompt('请输入标签名称:');
+      let id = window.prompt('请输入标签id:');
+      let text = window.prompt('请输入标签内容：');
+
+      name && id && text && this.list.push({
+        name: name,
+        id:id,
+        text:text
+      });
+    }
+    if (btnKey == 'del'){
+      this.list.splice(this.selectTabIndex, 1);
+    }
+    if (btnKey == 'delone'){
+      let tab = this.list[this.selectTabIndex];
+
+      this.list = [tab];
+    }
   }
 
 }
